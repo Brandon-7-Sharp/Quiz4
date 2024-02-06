7372d794420b10da1adf72fe13264eb1ba6470fe
Brandon Sharp <bmssharp2002@gmail.com>
2024-02-05 19:33:25 -0600

    Adding dataclass file

---
+++ dataclass.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+@dataclass
+class Star:
+    name: str
+    mass: int
+    diameter: float
+
+    def implode(self) -> None:
+        self.mass /= 10
+        self.diameter /= 20
+
+def main():
+
+    
+    sol = Star("Sol", 1000, 225)
+
+    print("Name: " + sol.name)
+    print("Mass: " + str(sol.mass))
+    print("Diameter: " + str(sol.diameter))
+
+    sol.implode()
+
+    print("Name: " + sol.name)
+    print("Mass: " + str(int(sol.mass)))
+    print("Diameter: " + str(sol.diameter))
+
+
+if __name__=='__main__': 	
+    main()
