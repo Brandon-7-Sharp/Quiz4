a8cae7bbfdde94d9fdbacff34d1eca834a00ac3e
Brandon Sharp <bmssharp2002@gmail.com>
2024-02-05 19:35:39 -0600

    Adding no_property file

---
+++ no_property.py
@@ -0,0 +1,18 @@
+from abc import ABC
+
+
+class Clown:
+    def __init__(self):
+        self._makeup = "On"
+
+    def get_makeup(self) -> str:
+        return self._makeup
+
+def main():
+    clown = Clown()
+    print(clown.get_makeup())
+    
+
+
+if __name__=='__main__': 	
+    main()
